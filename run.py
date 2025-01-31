6ff491a349a60c18de6259ac9928a9047bde5992
AlperlavKirill <kirill@alperlav.com>
2025-02-01 02:17:23 +0300

    update logging

--- run.py
+++ run.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python3
-
+import sys
+import traceback
 from facefusion import core
+import logging
 
-if __name__ == '__main__':
-	print("in main")
-	core.wrapped_cli()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+if __name__ == 'main':
+    try:
+        logger.info("Запуск приложения...")
+        core.cli()
+    except Exception as e:
+        logger.error("Произошла критическая ошибка:")
+        logger.error(str(e))
+        logger.error("Полный стек вызовов:")
+        logger.error(traceback.format_exc())
+        raise
