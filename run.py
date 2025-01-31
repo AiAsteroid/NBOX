6e65b51f41fcb4b9a7a47b069cd88e3cfc7e5098
AlperlavKirill <kirill@alperlav.com>
2025-02-01 01:43:36 +0300

    log

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 from facefusion import core
-from huggingface_hub import snapshot_download
 
 if __name__ == '__main__':
-	# local_dir = "./.assets/models"
-	# model = 'wanesoft/faceswap_pack'
-	# snapshot_download(repo_id=model, local_dir=local_dir, local_files_only=False)
+	print("in main")
 	core.cli()
